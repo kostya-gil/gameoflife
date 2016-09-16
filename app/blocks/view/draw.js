003551c0e1fc769a6fffe08a3f2f700e1601d162
kostya-gil <kostgil@mail.ru>
2016-09-16 23:11:24 +0700

    add testing version game

---
+++ app/blocks/view/draw.js
@@ -0,0 +1,113 @@
+import Cell from '../model/cell';
+import Board from '../model/board';
+/**
+ * Константы, определяющие изначальное количество клеток на поле
+ */
+const MAX_X = 40;
+const MAX_Y = 40;
+
+/**
+ * Константа для резмера клетки
+ */
+const SIZE_CELL= 25;
+
+/**
+ * Константы,определяющие цвет живой и мертвой клетки (зеленая, красная)
+ */
+const ALIVE_CELL_COLOR = 'green';
+const DEATH_CELL_COLOR = 'red';
+/**
+ * Класс отрисовки игрового поля
+ * @class Cell
+ * @param {Object} board
+ * @param {Object} canvas
+ */
+export default class Draw {
+	
+	constructor(element) {
+		this.board = new Board();
+		this.cells = [];
+		
+		this.idInterval = null;
+		
+		this.element = document.getElementById(element);
+		this.canvas = this.element.getContext('2d');
+		
+		this.element.addEventListener('click', (event) => {
+			this.stopTick();
+			
+			let x = event.pageX - this.element.offsetLeft,
+					y = event.pageY - this.element.offsetTop;
+			this.onClickCell(x, y);
+			this.render();
+		});
+	}
+
+	initialization() {
+		for(let y = 0; y <= MAX_Y; y++) {
+			for(let x = 0; x <= MAX_X; x++) {
+				this.board.addCell(new Cell(x, y, Math.floor(Math.random() * (1 - 0 + 1)) + 0));
+			}
+		}
+	}
+	
+	onClickCell(x, y) {
+		this.cells.forEach((item, i, arr) => {
+			if (y > item.top && y < item.top + item.size && x > item.left && x < item.left + item.size) {
+				let cell = this.board.getCellAt(item.left/SIZE_CELL, item.top/SIZE_CELL);
+				if(item.color == ALIVE_CELL_COLOR) {
+					arr.splice(i, 1, {
+						color: DEATH_CELL_COLOR,
+						left: item.left,
+						top: item.top,
+						size: item.size
+					});
+					cell.state = 0;
+				} else {
+					arr.splice(i, 1, {
+						color: ALIVE_CELL_COLOR,
+						left: item.left,
+						top: item.top,
+						size: item.size
+					});
+					cell.state = 1;
+				}
+			}
+		});
+	}
+	
+	render() {
+		for(let y = 0; y <= MAX_Y; y++) {
+			for(let x = 0; x <= MAX_X; x++) {
+				let cell = this.board.getCellAt(x, y);
+				this.cells.push({
+					color: cell.isAlive() ? ALIVE_CELL_COLOR  : DEATH_CELL_COLOR,
+					left: cell.x * SIZE_CELL,
+					top: cell.y * SIZE_CELL,
+					size: SIZE_CELL
+				});
+			}
+		}
+		
+		this.cells.forEach((item) => {
+			this.canvas.beginPath();
+			this.canvas.rect(item.left, item.top, item.size,item.size);
+			this.canvas.fillStyle = item.color;
+			this.canvas.fill();
+			this.canvas.lineWidth = 3;
+			this.canvas.strokeStyle = 'black';
+			this.canvas.stroke();
+		});
+	}
+				
+	tick() {
+		this.idInterval = setInterval(() => {
+			this.board.nextStep();
+			this.render();
+		}, 1000);
+	}
+	
+	stopTick() {
+		clearInterval(this.idInterval);
+	}
+}
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,26 @@
-//Пусто
+import Draw from '../blocks/view/draw';
+
+((global)=> {
+	let btnStop = document.getElementById('stop');
+	let btnStart = document.getElementById('start');
+	let btnInit = document.getElementById('init');
+	
+	let draw = new Draw('temp');
+	
+	draw.initialization();
+	draw.render();
+	
+	btnInit.addEventListener('click', function() {
+		draw.initialization();
+		draw.render();
+		draw.tick();
+	});
+	
+	btnStart.addEventListener('click', function() {
+		draw.tick();
+	});
+	btnStop.addEventListener('click', function() {
+		draw.stopTick();
+	});
+	
+})(this);
